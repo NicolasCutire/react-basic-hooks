2aec87f773af01427677313a7c37dd0fbde92073
NicolasCutire <nicoalvarocutire@gmail.com>
2021-06-06 23:48:01 -0500

    avance commit

--- react_app_holamundo/src/App.js
+++ react_app_holamundo/src/App.js
@@ -1,6 +1,39 @@
 import logo from './logo.svg';
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './App.css';
+const style = {
+  table: {
+    borderColapse: "colapse"
+  },
+  tableCell: {
+    border: "1px solid gray",
+    margin: 0,
+    padding: "5px 10px",
+    width: "max-content",
+    minWidth: "150px"
+  },
+  form: {
+    container: {
+      padding: "20px",
+      border: "1px solid #F0F8FF",
+      borderRadius: "15px",
+      width: "max-content",
+      marginBottom: "40px"
+    },
+    inputs: {
+      marginBottom: "5px"
+    },
+    submitBtn: {
+      marginTop: "10px",
+      padding: "10px 15px",
+      border: "none",
+      backgroundColor: "lightseagreen",
+      fontSize: "14px",
+      borderRadius: "5px"
+    }
+  }
+};
+
 
 function Libro({numero,titulo, año }) {
   return (
@@ -48,6 +81,65 @@ function Counter(){
     </div>
   )
 }
+function FormBook({autores}){
+  const titleRef = useRef(null);
+  const yearRef = useRef(null);
+  const autorRef = useRef(null);
+  function addBook(e){ 
+    e.preventDefault();
+    const title = titleRef.current.value;
+    const year = yearRef.current.value;
+    const autor = autorRef.current.value;
+    const book = {title,year,autor}
+    console.log(book)  
+
+  }
+  return(
+    <form onSubmit={addBook} style={style.form.container}>
+      <label>Titulo</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        //className="userFirstname"
+        name="title"
+        type="text"
+        ref={titleRef}
+      />
+      <br />
+
+      <label>Year</label>
+      <br />
+      <input
+        style={style.form.inputs}
+        //className="userLastname"
+        name="year"
+        type="text"
+        ref={yearRef}
+      />
+      <br />
+      <labe>Autor</labe>
+      <br />
+      <select
+       style={style.form.inputs}
+       name ="select"
+       ref = {autorRef}
+       >
+        {autores.map( (a)=>
+          <option value={a.nombre}>{a.nombre}</option>    
+        )}        
+      </select>
+
+      <br />
+      <input
+        style={style.form.submitBtn}
+        //className="submitPhone"
+        type="submit"
+        value="Add book"
+      />
+      
+    </form>
+  )
+}
 function App() {
   const LibrosNikko = [
     {
@@ -59,10 +151,15 @@ function App() {
       año: 2080
     },
   ]
+  const Autores = [
+    {nombre: 'nikko',edad: 19,libros:[]},
+    {nombre: 'Willy',edad: 8,libros:[]},
+  ]
   return (
     <>
     <Autor nombre = "Nikko" edad = {25} libros = {LibrosNikko}/>
     <Counter/>
+    <FormBook autores = {Autores}/>
     </>
   );
 }
